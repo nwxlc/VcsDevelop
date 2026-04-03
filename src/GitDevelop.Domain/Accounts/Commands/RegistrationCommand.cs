7d04d3eefba61c1c41de0ed154a8f7688103d767
Kachurka Daniel <132569789+nwxlc@users.noreply.github.com>
2026-04-03 20:57:46 +0300

    Add registration. (#14)

---
+++ src/GitDevelop.Domain/Accounts/Commands/RegistrationCommand.cs
@@ -0,0 +1,27 @@
+namespace GitDevelop.Domain.Accounts.Commands;
+
+public sealed class RegistrationCommand
+{
+    public string Name { get; private init; }
+    public string Email { get; private init; }
+    public Password Password { get; private init; }
+
+    private RegistrationCommand(
+        string name,
+        string email,
+        Password password)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+
+    public static RegistrationCommand Create(string name, string email, Password password)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+        ArgumentNullException.ThrowIfNull(password);
+
+        return new RegistrationCommand(name, email, password);
+    }
+}
---
+++ src/GitDevelop.WebApi/Contracts/RegistrationRequest.cs
@@ -0,0 +1,8 @@
+namespace GitDevelop.WebApi.Contracts;
+
+public sealed class RegistrationRequest
+{
+    public string Name { get; init; } = null!;
+    public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
+}
