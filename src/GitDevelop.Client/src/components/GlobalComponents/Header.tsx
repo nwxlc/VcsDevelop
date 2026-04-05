991f0de38ec5063ca253d60b04b603fd876fc72c
Манкаш Василий <mankashvasiliy@gmail.com>
2026-04-06 02:20:20 +0300

    Страница авторизации, хендлеры

--- src/GitDevelop.Client/src/components/GlobalComponents/Header.tsx
+++ src/GitDevelop.Client/src/components/GlobalComponents/Header.tsx
@@ -1,25 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Button from "../../components/GlobalComponents/Button.tsx";
 import logo from "../../assets/images/logo.png";
+import { Link, useLocation } from "react-router";
 
-import {Link} from "react-router";
-
-
-class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <div className="header-title">
-                    <div className="header-logo"><img src={logo} alt=""/></div>
-                    <Link to="/"><h1>VCS-X</h1></Link>
+const Header = () => {
+    const location = useLocation();
+    
+    return (
+        <div className="header">
+            <div className="header-title">
+                <div className="header-logo">
+                    <img src={logo} alt="Logo" />
                 </div>
-                <Link to="/login">
-                    <Button label={"Авторизация"} onClick={()=>{}}/>  
-                </Link>
-                
+                <Link to="/"><h1>VCS-X</h1></Link>
             </div>
-        );
-    }
-}
+            {location.pathname === "/login" ? (<></>) : (
+                <Link to="/login">
+                <Button label={"Авторизация"} onClick={() => {}} />
+            </Link>
+            )}
+            
+        </div>
+    );
+};
 
 export default Header;
