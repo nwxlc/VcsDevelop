64558dd9ef01f742189bc872a68d395dd6a86748
nwxlc <jeffkiller04@mail.ru>
2026-04-13 11:09:49 +0300

    Добавил метод логин

---
+++ src/VcsDevelop.WebApi/Contracts/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace VcsDevelop.WebApi.Contracts;
+
+public sealed class LoginRequest
+{
+    public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
+}
--- src/VcsDevelop.WebApi/Controllers/UserController.cs
+++ src/VcsDevelop.WebApi/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using VcsDevelop.Application.Accounts.Entities;
 using VcsDevelop.Domain.Accounts;
 using VcsDevelop.Domain.Accounts.Commands;
 using VcsDevelop.WebApi.Contracts;
+using LoginRequest = VcsDevelop.WebApi.Contracts.LoginRequest;
 
 namespace VcsDevelop.WebApi.Controllers;
 
@@ -12,8 +13,8 @@ public class UserController : ControllerBase
 {
     [HttpPost("registration")]
     public async Task<ActionResult<AccountResponse>> RegistrationAsync(
-        [FromBody] RegistrationRequest request,
-        [FromServices] IRegistrationCommandHandler handler,
+        [FromBody]RegistrationRequest request,
+        [FromServices]IRegistrationCommandHandler handler,
         CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(request);
@@ -21,6 +22,24 @@ public class UserController : ControllerBase
 
         var command = RegistrationCommand.Create(
             request.Name,
+            request.Email,
+            Password.Create(request.Password));
+
+        var accountResponse = await handler.HandleAsync(command, cancellationToken).ConfigureAwait(false);
+
+        return Ok(accountResponse);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AccountResponse>> LoginAsync(
+        [FromBody]LoginRequest request,
+        [FromServices]ILoginCommandHandler handler,
+        CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        ArgumentNullException.ThrowIfNull(handler);
+
+        var command = LoginCommand.Create(
             request.Email,
             Password.Create(request.Password));
 
