7e853cb8dd6c7ee437570fb1e32618e505119d82
nwxlc <jeffkiller04@mail.ru>
2026-05-13 21:15:49 +0300

    Fix

--- src/VcsDevelop.Application/VcsObjects/Documents/Entities/Models/Paging/PagedResult.cs
+++ src/VcsDevelop.Application/VcsObjects/Documents/Entities/Models/Paging/PagedResult.cs
@@ -3,5 +3,5 @@ namespace VcsDevelop.Application.VcsObjects.Documents.Entities.Models.Paging;
 public sealed class PagedResult<T>
 {
     public IEnumerable<T> Data { get; set; } = [];
-    public PaginationMetadata Metadata { get; set; } = null;
+    public PaginationMetadata Metadata { get; set; } = new();
 }
